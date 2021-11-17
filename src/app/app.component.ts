fbb61a4b9af2469768745d11bc6e80daddef9c55
Daniel Simon <d.simon@onventis.de>
2021-11-17 11:05:41 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { myText } from 'reusable-stuff';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'storybook-ts-path-error-repro';
+  title = myText;
 }
